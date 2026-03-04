703350256b2b3d172897af8d389348a472404a91
Mohammad Ali Saeidi <1saeidima@gmail.com>
2026-03-04 13:21:39 +0400

    chore: add ag grid react

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,11 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { getQueryClient } from "@/lib/get-query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RowSelectionModule } from "ag-grid-community";
+import { AgGridProvider } from "ag-grid-react";
 import type * as React from "react";
+
+const modules = [RowSelectionModule];
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	const queryClient = getQueryClient();
@@ -12,7 +16,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<DirectionProvider dir="rtl" direction="rtl">
-				<TooltipProvider>{children}</TooltipProvider>
+				<TooltipProvider>
+					<AgGridProvider modules={modules}>{children}</AgGridProvider>
+				</TooltipProvider>
 			</DirectionProvider>
 			<ReactQueryDevtools />
 		</QueryClientProvider>
