ada4f7bec72fe2865e377a376e746ec4032f15e8
Mohammad Ali Saeidi <1saeidima@gmail.com>
2026-03-20 11:47:31 +0400

    feat(proxy): set "x-pathname" as the pathname of the user for server side logic usage

--- proxy.ts
+++ proxy.ts
@@ -1,23 +1,23 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-// import { setStoreSlugInCookieProxy } from "./proxies/setStoreSlugInCookieProxy";
 
 export function proxy(request: NextRequest) {
-  // setStoreSlugInCookieProxy(request);
+	const requestHeaders = new Headers(request.headers);
+	requestHeaders.set("x-pathname", request.nextUrl.pathname);
 
-  return NextResponse.next();
+	return NextResponse.next({ request: { headers: requestHeaders } });
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public-page (your custom excluded path)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico|public-page).*)",
-  ],
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - api (API routes)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 * - public-page (your custom excluded path)
+		 */
+		"/((?!api|_next/static|_next/image|favicon.ico|public-page).*)",
+	],
 };
