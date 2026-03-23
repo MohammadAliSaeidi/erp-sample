094eeba75da54dcb6a113e5e0e1c995abe4a2625
Mohammad Ali Saeidi <1saeidima@gmail.com>
2026-03-23 13:23:25 +0400

    refactor: rename get-categories-controller

--- features/categories/domain/controller/get-categories-controller.ts
+++ features/categories/domain/controller/get-categories-controller.ts
@@ -6,6 +6,6 @@ import prisma from "@/features/shared/lib/prisma";
 export async function getCategoriesController(request: NextRequest) {
   const { authContext } = request as WithAuthContext;
   const categoryRepository = buildCategoryRepository(prisma);
-  const categories = categoryRepository.getList(authContext.storeId);
+  const categories = await categoryRepository.getList(authContext.storeId);
   return NextResponse.json(categories);
 }
