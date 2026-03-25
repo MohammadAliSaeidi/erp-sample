47112f412fbcd6aeff6bda3d4ec0b95ade480a35
Mohammad Ali Saeidi <1saeidima@gmail.com>
2026-03-25 13:56:33 +0400

    fix: show spinner loader in categories page loading

--- app/store/[storeSlug]/inventory/categories/loading.tsx
+++ app/store/[storeSlug]/inventory/categories/loading.tsx
@@ -1,5 +1,11 @@
+import { SpinnerCustom } from "@/components/ui/spinner";
+
 function Loading() {
-	return <div className="flex h-full w-full bg-black">asd</div>;
+	return (
+		<div className="flex h-full w-full items-center justify-center">
+			<SpinnerCustom />
+		</div>
+	);
 }
 
 export default Loading;
